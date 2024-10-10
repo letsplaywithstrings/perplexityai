d152c545ecbd0861ab94a84c28ae9f3224217710
Ahmet Metin <32226861+letsplaywithstrings@users.noreply.github.com>
2024-10-10 21:11:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="perplexityai",
-    version="0.1",
-    description="A python api to use perplexity.ai",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/nathanrchn/perplexityai",
-    packages=find_packages(),
-    requires=["requests", "websocket-client"]
+name="perplexityai",
+version="0.1",
+description="A python api to use perplexity.ai",
+long_description=long_description,
+long_description_content_type="text/markdown",
+url="https://github.com/nathanrchn/perplexityai",
+packages=find_packages(),
+install_requires=["requests", "websocket-client"]
 )
